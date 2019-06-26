0760dff7db59de3ad5b5ab01fd2727af50ba6f56
khaled <khaled_amari@live.fr>
2019-06-26 15:16:48 +0100

    firstcommit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Admin from './admin' 
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Jasser the Webmaster</h1> 
+     <Admin />
+    </div>
+  );
+}
+
+export default App;
