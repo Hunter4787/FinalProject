fda7e04874352c96790069b1a24c65d5c8a7be34
khaled <khaled_amari@live.fr>
2019-06-28 16:31:09 +0100

    connexion modal 28/06

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,16 @@ import './App.css';
 import Nav from './nav' 
 import Admin from './admin'
 import Conexion from './connexion'
+import withFirebaseAuth from 'react-with-firebase-auth'
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import firebaseConfig from './firebaseConfig';
 
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firebaseAppAuth = firebaseApp.auth();
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
+};
 function App() {
   return (
     <div className="App">
@@ -14,4 +23,7 @@ function App() {
   );
 }
 
-export default App;
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth,
+})(App);
