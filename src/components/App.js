d2e308c1f418d7120cedc38684ec6913cea0da82
slah sbi <slahsbi@gmail.com>
2019-06-28 09:05:50 +0100

    changement jeu/27

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
-
 import Nav from './nav' 
 
 function App() {
   return (
     <div className="App">
-     <h1>Jasser the Webmaster</h1> 
      <Nav />
     </div>
   );
