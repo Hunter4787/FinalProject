fe14b718c0cacde41f2ae0e6695b1f4f1ec62798
slah sbi <slahsbi@gmail.com>
2019-07-02 16:52:11 +0100

    updated logo

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom'
 class Footer extends Component {
     constructor(props) {
         super(props);
@@ -12,24 +13,22 @@ class Footer extends Component {
                     <div className="row">
                         <div className="col-md-4">
                             <div className="widget widget_text">
-                                <h2 className="widget-title"><span>ABOUT US</span></h2>
+                                <h2 className="widget-title"><span>À propos</span></h2>
                                 <div className="textwidget">
-                                    <a className="navbar-brand" href="index-building.html"><img src="images/logo-icon.png" alt="image" /><span>Constructed</span></a>
+                                    <a className="navbar-brand" href="index-building.html"><img src="images/logo.png" alt="image" /><span>Sanعa</span></a>
                                     <p>Aenean suscipit eget mi act fermentum phasellus vulputate turpis tincidunt. Aenean suscipit eget. Aenean suscipit eget mi act fermentum phasellus vulputate turpis tincidunt. Aenean suscipit ege  Aenean suscipit eget mi act fermentum phasellus.</p>
                                 </div>
                             </div>
                         </div>
                         <div className="col-md-4">
                             <div className="widget widget_links">
-                                <h2 className="widget-title"><span>COMPANY LINKS</span></h2>
+                                <h2 className="widget-title"><span>Sanعa</span></h2>
                                 <ul className="wprt-links clearfix col2">
-                                    <li><a data-scroll href="#home">Home</a></li>
-                                    <li><a data-scroll href="#about">About Us</a></li>
-                                    <li><a data-scroll href="#services">Services</a></li>
-                                    <li><a data-scroll href="#projects">Our Work</a></li>
-                                    <li><a data-scroll href="#testimonials">Testimonials</a></li>
-                                    <li><a data-scroll href="#price">Price</a></li>
-                                    <li><a data-scroll href="#contact">Contact Us</a></li>
+                                <li><Link to="/" data-scroll >Acceuil</Link></li>
+                                <li><Link to="/a-propos" data-scroll >À propos</Link></li>
+                                <li><Link to="/inscription" data-scroll >Inscription</Link></li>
+                                <li><Link to="/connexion" data-scroll >Se connecter</Link></li>  
+                                <li><Link to="/contact" data-scroll >Contact</Link></li>
                                 </ul>
                             </div>
                         </div>
@@ -43,7 +42,7 @@ class Footer extends Component {
                                     </li>
                                     <li className="phone clearfix">
                                         <span className="hl">Phone:</span>
-                                        <span className="text">+56 (0) 012 345 6789</span>
+                                        <span className="text">+216 00 000 000</span>
                                     </li>
                                     <li className="email clearfix">
                                         <span className="hl">E-mail:</span>
@@ -53,11 +52,11 @@ class Footer extends Component {
                             </div>
                             <div className="widget widget_socials">
                                 <div className="socials">
-                                    <a target="_blank" href="#"><i className="fa fa-twitter" /></a>
-                                    <a target="_blank" href="#"><i className="fa fa-facebook" /></a>
-                                    <a target="_blank" href="#"><i className="fa fa-google-plus" /></a>
-                                    <a target="_blank" href="#"><i className="fa fa-pinterest" /></a>
-                                    <a target="_blank" href="#"><i className="fa fa-dribbble" /></a>
+                                    <a target="_blank" href="#"><i className="fa fa-facebook" aria-hidden="true" /></a>
+                                    <a target="_blank" href="#"><i className="fa fa-twitter" aria-hidden="true" /></a>
+                                    <a target="_blank" href="#"><i className="fa fa-instagram" aria-hidden="true" /></a>
+                                    <a target="_blank" href="#"><i className="fa fa-youtube" aria-hidden="true" /></a>
+                                
                                 </div>
                             </div>
                         </div>
@@ -68,7 +67,7 @@ class Footer extends Component {
                         <div id="bottom-bar-inner" className="wprt-container">
                             <div className="bottom-bar-inner-wrap">
                                 <div className="bottom-bar-content">
-                                    <div id="copyright">All Rights Reserved. Company Name © 2018</div>
+                                    <div id="copyright">All Rights Reserved. Sanعa © 2019</div>
                                     {/* /#copyright */}
                                 </div>
                                 {/* /.bottom-bar-content */}
